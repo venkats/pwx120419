ef8e732429e81af5fb50b54edb755f0d744cf5ea
Venkat Subramaniam <venkats@agiledeveloper.com>
2019-12-04 12:23:55 -0500

    prototypal inheritance

---
+++ examples/part3/10_prototypal_inheritance/sample.js
@@ -0,0 +1,31 @@
+class Person {
+  constructor(first, last) {
+    this.first = first;    
+    this.last = last;
+  }                    
+  
+  toString() {
+    return `Person: ${this.first} ${this.last}`;
+  }
+}                           
+
+//constructor is given we don't have to write one.
+class CoolPerson extends Person {
+  toString() {
+    return `Cool${super.toString()}`;
+  }                                  
+}
+
+const alan = new CoolPerson('Alan', 'Turning');
+console.log(alan);
+let proptotype = Object.getPrototypeOf(alan);
+console.log(proptotype);
+
+proptotype = Object.getPrototypeOf(proptotype);
+console.log(proptotype);
+
+proptotype = Object.getPrototypeOf(proptotype);
+console.log(proptotype);
+
+proptotype = Object.getPrototypeOf(proptotype);
+console.log(proptotype);
