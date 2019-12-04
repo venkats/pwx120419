3f29762c64d96d86d1d9972bdb38dc7c101a285f
Venkat Subramaniam <venkats@agiledeveloper.com>
2019-12-04 09:17:26 -0500

    triple equals

---
+++ examples/part1/04_triple_equals/sample.js
@@ -0,0 +1,10 @@
+const a = "1";
+const b = 1;
+const c = "1.0";
+
+console.log(a === b);
+console.log(b === c);
+console.log(a === c);
+                                                         
+// === straigh up comparison without any conversion
+//eslint can help to verify that we use === instead of ==
